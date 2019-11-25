9d28abeaaa08f6619568148d697c6f315b242919
George Nassar <gnassar@ualberta.ca>
2019-11-24 18:18:32 -0700

    Hopefully this works

--- indexing.py
+++ indexing.py
@@ -5,6 +5,7 @@ def sort_files():
     os.system('sort -u -o ./parsed/terms.txt ./parsed/terms.txt')
     os.system('sort -u -o ./parsed/emails.txt ./parsed/emails.txt')
     os.system('sort -u -o ./parsed/dates.txt ./parsed/dates.txt')
+    os.system('sort -u -o ./parsed/recs.txt ./parsed/recs.txt')
 
 def create_date_index():
     os.system('sh ./scripts/load-dates.sh')
@@ -16,9 +17,12 @@ def create_terms_index():
 def create_emails_index():
     os.system('sh ./scripts/load-emails.sh')
 
+def create_records_index():
+    os.system('sh ./scripts/load-records.sh')
 
 def create_idx_files():
     sort_files()
     create_date_index()
     create_emails_index()
     create_terms_index()
+    create_records_index()
